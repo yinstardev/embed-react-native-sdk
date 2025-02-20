62a83abeff612bf316a2b51c3c771581d6c17c33
Prashant.patil <prashant.patil@hyd-prashant.pa>
2025-02-20 12:25:50 +0530

    imports fix check

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,3 @@ import { init } from "./init";
 import { LiveboardEmbedClassX } from "./LiveboardEmbedClass";
 
 export { init, LiveboardEmbedClassX };
-
-const EmbedReactNativeSDK = {
-  init,
-};
-export default EmbedReactNativeSDK;
