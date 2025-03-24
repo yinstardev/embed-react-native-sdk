cb2e131fbbee9f1962365bde3e8887bdd9216aa8
Prashant.patil <prashant.patil@hyd-prashant.pa>
2025-03-24 15:38:25 +0530

    conv embed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
+import { ConversationEmbed } from "./ConversationEmbed";
 import { useLiveboardRef } from "./hooks/useLiveboardRef";
 import { init } from "./init";
 import { LiveboardEmbed, LiveboardEmbedRef } from "./LiveboardEmbed";
 import { Action, AuthType, ViewConfig, HostEvent } from "./types";
 
-export { init, LiveboardEmbed, useLiveboardRef, AuthType, ViewConfig, Action, HostEvent };
+export { init, ConversationEmbed, LiveboardEmbed, useLiveboardRef, AuthType, ViewConfig, Action, HostEvent };
 export type { LiveboardEmbedRef };
 
 const EmbedReactNativeSDK = {
   init,
   LiveboardEmbed,
   useLiveboardRef,
+  ConversationEmbed
 };
 export default EmbedReactNativeSDK;
