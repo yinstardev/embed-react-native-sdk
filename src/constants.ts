9438aad23a6b4023483d55c10409120423442bf7
Prashant.patil <prashant.patil@hyd-prashant.pa>
2025-03-25 05:35:56 +0530

    conv embed

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,17 @@
 import { WebViewProps } from 'react-native-webview';
 
-export const VERCEL_SHELL_URL = "https://mobile-embed-shell.vercel.app";
+export const VERCEL_SHELL_URL = "https://embed-vercel-shell-git-class-based-final-yinstardevs-projects.vercel.app";
 
 export enum MSG_TYPE {
-    INIT = "INIT",
-    EMBED = "EMBED",
-    INIT_VERCEL_SHELL = "INIT_VERCEL_SHELL",
-    REQUEST_AUTH_TOKEN = "REQUEST_AUTH_TOKEN",
-    EMBED_EVENT = "EMBED_EVENT",
-    HOST_EVENT_REPLY = "HOST_EVENT_REPLY",
-    EMBED_EVENT_REPLY = "EVENT_REPLY",
-    HOST_EVENT = "HOST_EVENT",
-    AUTH_TOKEN_RESPONSE = "AUTH_TOKEN_RESPONSE",
+  INIT = "INIT",
+  EMBED = "EMBED",
+  INIT_VERCEL_SHELL = "INIT_VERCEL_SHELL",
+  REQUEST_AUTH_TOKEN = "REQUEST_AUTH_TOKEN",
+  EMBED_EVENT = "EMBED_EVENT",
+  HOST_EVENT_REPLY = "HOST_EVENT_REPLY",
+  EMBED_EVENT_REPLY = "EVENT_REPLY",
+  HOST_EVENT = "HOST_EVENT",
+  AUTH_TOKEN_RESPONSE = "AUTH_TOKEN_RESPONSE",
 }
 
 export const DEFAULT_WEBVIEW_CONFIG: WebViewProps = {
